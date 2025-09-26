2ba6629635c8bebddbb2b2ceda4ca37ec7a1d3b3
unknown <nwosumick@gmail.com>
2025-09-26 10:40:50 +0100

    initial commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { toast } from 'react-toastify' 
+import { motion } from 'framer-motion';
 
 const Contact = () => {
-
-      const [result, setResult] = React.useState("");
+ 
+        const [result, setResult] = React.useState("");
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
-   
     const formData = new FormData(event.target);
-    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    formData.append("access_key", "f109e083-ca9b-4b4b-b42b-2d054d85e548");
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -31,7 +32,12 @@ const Contact = () => {
   };
 
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+    <motion.div 
+       initial={{opacity: 0,x:-200}}
+       transition={{duration: 1}}
+       whileInView={{opacity: 1, x:0}}
+       viewport={{once: true}}
+    className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
        <h1 className='text-2x1 sm:text-4x1 font-bold mb-2 text-center px-1'>Contact <span className='underline underline-offset-4 decoration-1 under font-light '>With Us</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together</p>
 
@@ -53,7 +59,7 @@ const Contact = () => {
           <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>{result ? result : "Send Message"}</button>
       </form>
          
-    </div>
+    </motion.div>
 
   )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,7 @@
 import { useState} from 'react'
 import { assets, projectsData } from '../assets/assets'
 import { useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
 
@@ -28,7 +29,13 @@ const Projects = () => {
             setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1)
       }
   return (
-    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    <motion.div 
+       initial={{opacity: 0,x:-200}}
+       transition={{duration: 1}}
+       whileInView={{opacity: 1, x:0}}
+       viewport={{once: true}}
+    
+    className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2x1 sm:text-4x1 font-bold mb-2 text-center'>Projects <span className=' underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
 
@@ -66,7 +73,7 @@ const Projects = () => {
                   ))}
               </div>
           </div>
-    </div>
+    </motion.div>
 
 
   )
